62b7d90cd7d019bc0638d8501f8ee1c030ead3af
Amy Westcott <amy.westcott@yahoo.com>
2019-06-10 01:54:31 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -3,13 +3,14 @@ def dictionary
   shorten = {"hello" => "hi", "to" => "2", "two" => "2", "too" => "2", "for" => "4", "four" => "4", "be" => "b",
   "you" => "u", "at" => "@", "and" => "&"}
 end
+
 def word_substituter(string)
  array = string.split(" ")
  shorten = dictionary.keys
  array.each_with_index do |word, index|
     shorten.each do |change|
       if word.downcase == change
-      array[index] = dictionary[word]
+        array[index] = dictionary[word]
       end  
  end
  index += 1 
