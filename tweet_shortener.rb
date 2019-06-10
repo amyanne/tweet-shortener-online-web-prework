a15e025d60b2430cbcd580461b05fd020fbcee35
Amy Westcott <amy.westcott@yahoo.com>
2019-06-10 01:35:29 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -9,7 +9,7 @@ def word_substituter(string)
  index = 0
  shorten.each do |word|
     if array[index] = word
-      array[array.index(index)]
+      array[array.index(index)] = dictionary[word]
      
    end  
  end
